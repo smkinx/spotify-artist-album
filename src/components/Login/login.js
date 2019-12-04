6bef113da2c7db4c4fc383f3f93f981bb10467a4
Supratim Baruah <baruah.sm@gmail.com>
2019-12-03 19:46:09 -0500

    initial commit, set up base app

---
+++ src/components/Login/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@material-ui/core/Button'
+
+class Login extends React.Component {
+  constructor() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Button>Login With Spotify</Button>
+      </div>
+    )
+  }
+}
