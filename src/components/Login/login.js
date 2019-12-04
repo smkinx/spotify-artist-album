23875166db9940e93523376e1c8e7e831c2feb1a
Supratim Baruah <baruah.sm@gmail.com>
2019-12-03 22:23:30 -0500

    add auth functionality to login view

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button'
 
-class Login extends React.Component {
-
-  render() {
+const Login = () => {
     return (
       <div>
-        <Button color="green">Login With Spotify</Button>
+        <Button onClick={ ()=>{ window.open('https://accounts.spotify.com/authorize?client_id=868f22d60c3645cdaf1008175fc0d37d&redirect_uri=http:%2F%2Flocalhost:3000%2Fsearch&response_type=token',"_self") }}>Login With Spotify</Button>
       </div>
     )
-  }
-}
 
+}
 
 export default Login;
