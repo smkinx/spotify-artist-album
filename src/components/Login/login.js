aa102d5c45de73050eb6d2fdab3e4ce68063afbf
Supratim Baruah <baruah.sm@gmail.com>
2019-12-05 00:56:15 -0500

    update login page style

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -6,8 +6,8 @@ import Grid from '@material-ui/core/Grid';
 
 const Login = () => {
     return (
-      <Grid container alignContent="center" alignItems="center">
-        <Grid item>
+      <Grid className="login-container" container alignContent="center" alignItems="center">
+        <Grid item xs={12}>
           <Button className="button-login sm-button" onClick={ ()=>{ window.open('https://accounts.spotify.com/authorize?client_id=868f22d60c3645cdaf1008175fc0d37d&redirect_uri=http:%2F%2Flocalhost:3000%2Fsearch&response_type=token',"_self") }}>Login using Spotify</Button>
         </Grid>
       </Grid>
