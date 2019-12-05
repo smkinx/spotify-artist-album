4021637f31f6c3bb4929635552533039ac85dc41
Supra <baruah.sm@gmail.com>
2019-12-05 02:40:41 -0500

    remove unused files

--- src/components/Album/albumCard.js
+++ src/components/Album/albumCard.js
@@ -3,7 +3,6 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom'
 import './style.css'
 
 const AlbumCard = (props) => {
